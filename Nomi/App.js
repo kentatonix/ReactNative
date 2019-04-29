2242f1e525fb5208baee8c95d73e1126ed4fc228
kenta <techlax8@gmail.com>
2019-04-29 16:33:44 +0900

    amplify

--- Nomi/App.js
+++ Nomi/App.js
@@ -1,23 +1,19 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { withAuthenticator } from 'aws-amplify-react-native'; // or 'aws-amplify-react-native';
+import Amplify from 'aws-amplify';
+// Get the aws resources configuration parameters
+import aws_exports from './aws-exports'; // if you are using Amplify CLI
 
-export default class App extends React.Component {
+Amplify.configure(aws_exports);
+
+
+class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Open up App.js to start working app!!!!!!!!!!</Text>
+      <View>
+        <Text>Test</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-});
+export default withAuthenticator(App);
