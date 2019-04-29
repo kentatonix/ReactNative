dd33622a0579a6517601e9e6558c3c87016a28c3
kenta <techlax8@gmail.com>
2019-04-29 17:23:08 +0900

    amplify auth完了

--- Nomi/App.js
+++ Nomi/App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
 import { withAuthenticator } from 'aws-amplify-react-native'; // or 'aws-amplify-react-native';
 import Amplify from 'aws-amplify';
 // Get the aws resources configuration parameters
